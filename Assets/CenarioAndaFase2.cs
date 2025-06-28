3ec5f02a554fb2df088b64b76440303cbfe5c312
antiiiinio <roantonioaraujo@gmail.com>
2025-06-28 00:05:38 -0300

    cenario segunda fase com colisao e tudo

---
+++ Assets/CenarioAndaFase2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CenarioAndaFase2 : MonoBehaviour
+{
+          public Rigidbody cenario;
+    private float forceAmount = 500f;
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        cenario.AddForce(Vector3.back * forceAmount * Time.deltaTime);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "VoltaCenario")
+        {
+            cenario.position = new Vector3(cenario.position.x, cenario.position.y, 3404);
+        }
+    }
+}
+
